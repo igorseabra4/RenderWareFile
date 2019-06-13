6c6ba6ecff2a12cc8f3fd0e9a9b9f4a7788b4ec4
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2019-06-12 23:29:53 -0300

    import texture native from byte[]

--- RenderWareFile/Sections/TextureNative_0015.cs
+++ RenderWareFile/Sections/TextureNative_0015.cs
@@ -8,17 +8,38 @@ namespace RenderWareFile.Sections
     {
         public TextureNativeStruct_0001 textureNativeStruct;
         public Extension_0003 textureNativeExtension;
-        
+
         public TextureNative_0015 Read(BinaryReader binaryReader)
         {
             sectionIdentifier = Section.TextureNative;
             sectionSize = binaryReader.ReadInt32();
             renderWareVersion = binaryReader.ReadInt32();
-            
+
             Section textureNativeStructSection = (Section)binaryReader.ReadInt32();
             if (textureNativeStructSection != Section.Struct) throw new Exception(binaryReader.BaseStream.Position.ToString());
             textureNativeStruct = new TextureNativeStruct_0001().Read(binaryReader);
-            
+
+            Section textureNativeExtensionSection = (Section)binaryReader.ReadInt32();
+            if (textureNativeExtensionSection == Section.Extension)
+                textureNativeExtension = new Extension_0003().Read(binaryReader);
+
+            return this;
+        }
+
+        public TextureNative_0015 FromBytes(byte[] data)
+        {
+            BinaryReader binaryReader = new BinaryReader(new MemoryStream(data));
+
+            sectionIdentifier = (Section)binaryReader.ReadInt32();
+            if (sectionIdentifier != Section.TextureNative) throw new Exception(binaryReader.BaseStream.Position.ToString());
+
+            sectionSize = binaryReader.ReadInt32();
+            renderWareVersion = binaryReader.ReadInt32();
+
+            Section textureNativeStructSection = (Section)binaryReader.ReadInt32();
+            if (textureNativeStructSection != Section.Struct) throw new Exception(binaryReader.BaseStream.Position.ToString());
+            textureNativeStruct = new TextureNativeStruct_0001().Read(binaryReader);
+
             Section textureNativeExtensionSection = (Section)binaryReader.ReadInt32();
             if (textureNativeExtensionSection == Section.Extension)
                 textureNativeExtension = new Extension_0003().Read(binaryReader);
