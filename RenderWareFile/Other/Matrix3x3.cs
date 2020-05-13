60239a15b07b8d4d8cf0ab96044e66aa1f821a96
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2020-05-13 10:48:00 -0300

    add matrix identity

--- RenderWareFile/Other/Matrix3x3.cs
+++ RenderWareFile/Other/Matrix3x3.cs
@@ -11,5 +11,18 @@
         public float M31;
         public float M32;
         public float M33;
+
+        public static Matrix3x3 Identity => new Matrix3x3()
+        {
+            M11 = 1f,
+            M12 = 0f,
+            M13 = 0f,
+            M21 = 0f,
+            M22 = 1f,
+            M23 = 0f,
+            M31 = 0f,
+            M32 = 0f,
+            M33 = 1f,
+        };
     }
 }
