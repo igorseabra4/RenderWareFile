b83c4e8f4ad79062012d61bba60bf799ddb13691
igorseabra4 <igor.seabra@ccc.ufcg.edu.br>
2020-05-17 23:43:15 -0300

    add debug code for material list

--- RenderWareFile/Sections/MaterialList_0008.cs
+++ RenderWareFile/Sections/MaterialList_0008.cs
@@ -16,18 +16,31 @@ namespace RenderWareFile.Sections
             sectionSize = binaryReader.ReadInt32();
             renderWareVersion = binaryReader.ReadInt32();
 
+            int startOfSection = (int)binaryReader.BaseStream.Position;
+
             Section materialListStructSection = (Section)binaryReader.ReadInt32();
             if (materialListStructSection != Section.Struct) throw new Exception();
             materialListStruct = new MaterialListStruct_0001().Read(binaryReader);
 
+#if DEBUG
+            List< Material_0007> materials = new List<Material_0007>();
+            for (int i = 0; binaryReader.BaseStream.Position < startOfSection + sectionSize; i++)
+            {
+                Section materialSection = (Section)binaryReader.ReadInt32();
+                if (materialSection != Section.Material) throw new Exception();
+                materials.Add(new Material_0007().Read(binaryReader));
+            }
+            materialList = materials.ToArray();
+            materialListStruct.materialCount = materialList.Length;
+#else
             materialList = new Material_0007[materialListStruct.materialCount];
-
             for (int i = 0; i < materialListStruct.materialCount; i++)
             {
                 Section materialSection = (Section)binaryReader.ReadInt32();
                 if (materialSection != Section.Material) throw new Exception();
                 materialList[i] = new Material_0007().Read(binaryReader);
             }
+#endif
 
             return this;
         }
